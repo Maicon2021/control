d75f7b5c0b6fa998132b5e65d636e523b67f5291
SISTRAN\maicon.andreis <maicon.andreis@sistran.com.br>
2025-01-30 17:31:43 -0300

    Finalizado validações da classe CNPJ

--- domain/src/main/java/controleestoque/domain/vo/Cnpj.java
+++ domain/src/main/java/controleestoque/domain/vo/Cnpj.java
@@ -23,9 +23,33 @@ public class Cnpj extends ValueObject {
             throw new RuntimeException("CNPJ deve conter 14 dígitos numéricos");
         }
 
+        if (valeu.matches("(\\d)\\1{13}")) {
+            throw new RuntimeException("CNPJ inválido, sequência repetida");
+        }
+
+        int[] p1 = {5,4,3,2,9,8,7,6,5,4,3,2};
+        int[] p2 = {6,5,4,3,2,9,8,7,6,5,4,3,2};
+
+        int digito1 = verificador(valeu.substring(0,12), p1);
+        int digito2 = verificador(valeu.substring(0,12) + digito1, p2);
+
+        if (!valeu.equals(valeu.substring(0,12) + digito1 + digito2)) {
+            throw new RuntimeException("CNPJ inválido, dígitos verificadores não conferem");
+        }
 
 
 
+    }
+
+
+
+    private int verificador(String base, int[] p) {
+        int soma = 0;
+        for (int i = 0; i < p.length; i++) {
+            soma += (base.charAt(i) - '0') * p[i];
+        }
+        int resto = soma % 11;
+        return (resto < 2) ? 0 : (11 - resto);
     }
 
     public String getNumeroCnpj(){
