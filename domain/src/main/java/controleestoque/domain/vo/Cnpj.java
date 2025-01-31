937f6782faa412ca4ba42b0adc5a31f15d8349b6
Maicon Rodrigo Andreis <maicon.andreis@sistran.com.br>
2025-01-30 21:37:18 -0300

    CorrecÃção Variavel Value

--- domain/src/main/java/controleestoque/domain/vo/Cnpj.java
+++ domain/src/main/java/controleestoque/domain/vo/Cnpj.java
@@ -17,23 +17,23 @@ public class Cnpj extends ValueObject {
             throw new RuntimeException("CNPJ não pode ser nulo");
         }
 
-        final var valeu = this.numeroCnpj.replaceAll("[^0-9]", "");
+        final var value = this.numeroCnpj.replaceAll("[^0-9]", "");
 
-        if (valeu.length() != 14) {
+        if (value.length() != 14) {
             throw new RuntimeException("CNPJ deve conter 14 dígitos numéricos");
         }
 
-        if (valeu.matches("(\\d)\\1{13}")) {
+        if (value.matches("(\\d)\\1{13}")) {
             throw new RuntimeException("CNPJ inválido, sequência repetida");
         }
 
         int[] p1 = {5,4,3,2,9,8,7,6,5,4,3,2};
         int[] p2 = {6,5,4,3,2,9,8,7,6,5,4,3,2};
 
-        int digito1 = verificador(valeu.substring(0,12), p1);
-        int digito2 = verificador(valeu.substring(0,12) + digito1, p2);
+        int digito1 = verificador(value.substring(0,12), p1);
+        int digito2 = verificador(value.substring(0,12) + digito1, p2);
 
-        if (!valeu.equals(valeu.substring(0,12) + digito1 + digito2)) {
+        if (!value.equals(value.substring(0,12) + digito1 + digito2)) {
             throw new RuntimeException("CNPJ inválido, dígitos verificadores não conferem");
         }
 
